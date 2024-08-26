4d9056ac664684e8cb5ce3494ac3461040359d02
AbdelkarimDouadjia <abdelkarim.douadjia@gmail.com>
2024-08-26 11:29:17 +0100

    add the files

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import clsx from "clsx";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Light Saas Landing Page",
+  description: "Template created by Frontend Tribe",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="relative">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        {/* Other meta tags can go here */}
+      </head>
+      <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+        {children}
+      </body>
+    </html>
+  );
+}
